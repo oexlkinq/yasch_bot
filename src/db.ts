b54e88da11c1a84494d596240999f276f33fd689
yyko <68428103+oexlkinq@users.noreply.github.com>
2024-10-08 15:46:27 +0500

    исправлено состояние гонки при добавлении юзера в бд

--- src/db.ts
+++ src/db.ts
@@ -34,7 +34,35 @@ export class User {
         let user = res.rows[0]
 
         if (!user) {
-            res = await pool.query<dbUser>('INSERT INTO users (id, platform, notify, format, group_name, query) VALUES ($1::bigint, $2::text, true, 0, null, null) RETURNING *', [id, platform]);
+            // получить отдельного клиента
+            const client = await pool.connect()
+            // начать транзакцию
+            await client.query('begin')
+            
+            // id для tg - 0, для vk - 1
+            const platformId = (platform === 'tg') ? 0 : 1
+
+            // попытаться получить исключительную блокировку в рамках транзакции
+            // ключ для блокировки формируется из id пользователя и номера платформы, который записывается в диапазон с 63 по 59 разряды в id
+            const lockRes = await client.query<{ ok: boolean }>('SELECT pg_try_advisory_xact_lock(overlay(($1::bigint)::bit(64) PLACING ($2::int)::bit(4) FROM 2)::bigint) as ok', [id, platformId])
+            const lockOk = lockRes.rows[0].ok
+
+            if (lockOk) {
+                // при удачном получении исключительной блокировки, добавить пользователя в бд и записать в результат добавленные данные
+                res = await client.query<dbUser>('INSERT INTO users (id, platform, notify, format, group_name, query) VALUES ($1::bigint, $2::text, true, 0, null, null) RETURNING *', [id, platform])
+            } else {
+                // иначе ожидать завершения параллельного добавления с помощью ожидания разделяемой блокировки
+                await client.query('SELECT pg_advisory_xact_lock_shared(overlay(($1::bigint)::bit(64) PLACING ($2::int)::bit(4) FROM 2)::bigint)', [id, platformId])
+
+                // и по завершении получить добавленные параллельным запросом данные
+                res = await client.query<dbUser>('SELECT id, platform, notify, format, group_name, query FROM users WHERE id = $1::bigint and platform = $2::text', [id, platform])
+            }
+
+            // завершить транзакцию (также снимет блокировки)
+            await client.query('commit')
+            // вернуть клиента в пул
+            client.release()
+
             user = res.rows[0]
         }
 
@@ -57,26 +85,26 @@ export class User {
 
     async toggleMute() {
         const res = await this.pool.query<Pick<dbUser, 'notify'>>('UPDATE users SET notify = not notify WHERE id = $1::bigint AND platform = $2::text RETURNING notify', [this.id, this.platform])
-        
+
         return res.rows[0].notify
     }
-    
+
     async switchFormat() {
         const res = await this.pool.query<Pick<dbUser, 'format'>>('UPDATE users SET format = (format + 1) % 3 WHERE id = $1::bigint AND platform = $2::text RETURNING format', [this.id, this.platform])
-        
+
         return res.rows[0].format
     }
-    
+
     async setGroup(group: string) {
         await this.pool.query('UPDATE users SET group_name = $3::text WHERE id = $1::bigint AND platform = $2::text', [this.id, this.platform, group])
         this.group_name = group
     }
-    
+
     async setQuery(query: string) {
         await this.pool.query('UPDATE users SET query = $3::text WHERE id = $1::bigint AND platform = $2::text', [this.id, this.platform, query])
         this.query = query
     }
-    
+
     async wipe() {
         await this.pool.query('UPDATE users SET group_name = null, query = null WHERE id = $1::bigint AND platform = $2::text', [this.id, this.platform])
         this.group_name = undefined
